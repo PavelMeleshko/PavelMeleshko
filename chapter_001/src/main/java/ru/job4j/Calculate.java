29ba9a915febf2f659d74274568a0de106e79b8d
PavelMeleshko <ottotiger@mail.ru>
2017-11-29 14:38:34 +0300

    1.3 JanaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,11 @@
+/**
+* @author Pavel Meleshko (ottotige@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+ 
+
 package ru.job4j;
      public class Calculate{
         public static void main(String[] args){
