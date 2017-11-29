9522f590944000a0c5985877978d554bf898f5b5
PavelMeleshko <ottotiger@mail.ru>
2017-11-29 15:03:57 +0300

    Исправление ошибок в 1.3ем задании

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,26 @@
+package ru.job4j;
 /**
+* Class Класс моей первой программы 
 * @author Pavel Meleshko (ottotige@mail.ru)
-* @version $Id$
-* @since 0.1
+* @version 1
+* @since 29.11.2017
 */
+
+
+     public class Calculate{
+
+/**
+* Содержание
+* @param Hellow world.
+* @param метод для вывода первой надписи.
+*/
+
+
+        public static void main(String[] args){
+            System.out.println("Hellow World"); 
 
  
 
-package ru.job4j;
-     public class Calculate{
-        public static void main(String[] args){
-            System.out.println("Hellow World"); 
 
 }   
 }
