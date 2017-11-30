d7c3b95b548a90d45d2a8a8dadad19466faeda63
PavelMeleshko <ottotiger@mail.ru>
2017-11-30 23:01:23 +0300

    1.1 Создание Hello World. (исправление ошибок 2)

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,6 +1,6 @@
 package ru.job4j;
-      public class Calculate{
-         public static void main(String[] args){
-             System.out.println("Hellow World"); 
-         }   
-      } 
+public class Calculate{
+    public static void main(String[] args){
+        System.out.println("Hellow World"); 
+    }   
+} 
