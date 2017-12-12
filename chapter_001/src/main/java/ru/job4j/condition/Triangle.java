275e48db656f2ede1c5415b898c361c1e49a0866
PavelMeleshko <ottotiger@mail.ru>
2017-12-12 13:13:55 +0300

    3.3. Вычисление прлощади треугольника [#9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+/**
+ * @author Pavel Meleshko (ottotiger@mail.ru)
+ * @version 1
+ * @since 11.12.2017
+ */
+
+public class Triangle {
+   private Point a;
+   private Point b;
+   private Point c;
+
+   public Triangle(Point a, Point b, Point c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+   }
+
+
+
+   public double period(double ab, double ac, double bc) {
+      return (ab + ac + bc) / 2;
+   }
+
+   public double area() {
+      double rsl = -1;
+      double ab = this.a.distanceTo(this.b);
+      double ac = this.a.distanceTo(this.c);
+      double bc = this.b.distanceTo(this.c);
+      double p = this.period(ab, ac, bc);
+      if (this.exist(ab, ac, bc)) {
+         rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+      }
+      return rsl;
+   }
+ 
+   private boolean exist(double ab, double ac, double bc) {
+      return ab > 0 || ac > 0 || bc > 0 ? true : false;
+   }   
+}
