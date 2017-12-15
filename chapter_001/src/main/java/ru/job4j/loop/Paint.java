1d9511fe06010577f1690f85c99e3edfa4745316
PavelMeleshko <ottotiger@mail.ru>
2017-12-15 03:58:02 +0300

    4.4 Построить пирамиду в псевдографике [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,53 @@
+package ru.job4j.loop;
+/**
+ * @author Pavel Meleshko (ottotiger@mail.ru)
+ * @version 1
+ * @since 15.12.2017
+ */
+public class Paint {
+   public String rightTrl(int height) {
+      StringBuilder screen = new StringBuilder();
+      int weight = height;
+      for (int row = 0; row != height; row++) {
+         for (int column = 0; column != weight; column++) {
+            if (row >= column) {
+               screen.append("^");
+            } else {
+               screen.append(" ");
+            }
+         }
+         screen.append(System.lineSeparator());
+      }
+      return screen.toString();
+   }
+   public String leftTrl(int height) {
+     StringBuilder screen = new StringBuilder();
+     int weight = height;
+     for (int row = 0; row != height; row++) {
+         for (int column = 0; column != weight; column++) {
+             if (row >= weight - column - 1) {
+                 screen.append("^");
+             } else {
+                 screen.append(" ");
+             }
+         }
+         screen.append(System.lineSeparator());
+     }
+     return screen.toString();
+   }
+   public String pyramid(int height) {
+    StringBuilder screen = new StringBuilder();
+    int weight = 2 * height - 1;
+    for (int row = 0; row != height; row++) {
+        for (int column = 0; column != weight; column++) {
+            if (row >= height - column - 1 && row + height - 1 >= column) {
+                screen.append("^");
+            } else {
+                screen.append(" ");
+            }
+        }
+        screen.append(System.lineSeparator());
+    }
+    return screen.toString();
+}
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+/**
+ * @author Pavel Meleshko (ottotiger@mail.ru)
+ * @version 1
+ * @since 15.12.2017
+ */
+public class PaintTest {
+   @Test
+   public void whenPyramid4Right() {
+      Paint paint = new Paint();
+      String rst = paint.rightTrl(4);
+      System.out.println(rst);
+      assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator()) 
+.add("^   ")
+.add("^^  ")
+.add("^^^ ")
+.add("^^^^")
+.toString()));
+   }
+   @Test
+   public void whenPyramid4Left() {
+      Paint paint = new Paint();
+      String rst = paint.leftTrl(4);
+      System.out.println(rst);
+      assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+.add("   ^")
+.add("  ^^")
+.add(" ^^^")
+.add("^^^^")
+.toString()));
+   }
+}
+
