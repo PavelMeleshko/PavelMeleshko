a18ffc7cd2f743528b48d69c9a9242aefa6cd56a
PavelMeleshko <ottotiger@mail.ru>
2017-12-17 18:47:41 +0300

    4.4 Построить пирамиду в псевдографике [#4412]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,7 +1,7 @@
 package ru.job4j.loop;
 
 /**
- * @author Pavel Mekeshko (ottotiger@mail.ru)
+ * @author Pavel Meleshko (ottotiger@mail.ru)
  * @version 1
  * @since 14.12.2017
  */
@@ -10,25 +10,15 @@ public class Board {
       StringBuilder screen = new StringBuilder();
       String ln = System.lineSeparator();
       for (int i = 0; i < height; i++) {
-         if (i % 2 == 0) { 
             for (int j = 0; j < width; j++) {
-               if (j % 2 == 0) {
+               if ((j + i) % 2 == 0) {
                   screen.append("x");
                } else {
                   screen.append(" ");
                }
-            }
-		 } else {
-            for (int u = 0; u < width; u++) {
-               if (u % 2 == 0) {
-                  screen.append(" ");
-               } else {
-                  screen.append("x");
-               }
-            }
-         }
-		 screen.append(ln);
-		 }
-	  return screen.toString();
-	  }
-   }
+	     }
+             screen.append(ln);
+       }
+       return screen.toString();
+    }
+}
