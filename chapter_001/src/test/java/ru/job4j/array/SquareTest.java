440bc5ec7ec20690134ff9125c2abc81a1e271ca
PavelMeleshko <ottotiger@mail.ru>
2017-12-24 12:29:51 +0300

    5.0. Заполнить массив  степенями чисел. [#33488]

--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -10,14 +10,12 @@ import static org.junit.Assert.assertThat;
  * @since 20.12.2017
  */
 public class SquareTest {
-   @Test
-   public void checkingAnArrayFilledWithANumberInDegree() {
-        Square  = new Square();
-        int[] array = new int[5];
-           for (int i = 0; i < rst.length; i++) {
-              rst[i] = Math.pow(rst[i], 2);
-		   }
-		assertThat(rst, is(array[0] = 1, array[1] = 2, array[2] = 4, array[3] = 9, array[4] = 16)); 
-        
+    @Test
+    public void checkingAnArrayFilledWithANumberInDegree() {
+        Square sqr = new Square();
+        int[] rst = sqr.calculate(3);
+        int[] expect = {1, 4, 9};
+        assertThat(rst, is(expect));
+
     }
 }
